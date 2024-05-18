2a132d1e8e5ecae8f76621d465d6eb01c834f989
crrtd <pavlov07ivan07@yandex.ru>
2024-05-18 17:33:12 +0300

    добавлены @iBOutlet's и @iBAction

---
+++ Profile/ProfileViewController.swift
@@ -0,0 +1,20 @@
+import Foundation
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    @IBOutlet private var avatarImageView: UIImageView!
+    @IBOutlet private var nameLabel: UILabel!
+    @IBOutlet private var loginNameLabel: UILabel!
+    @IBOutlet private var descriptionLabel: UILabel!
+    
+    @IBOutlet private var logoutButton: UIButton!
+    
+    @IBAction private func didTapLogoutButton() {
+        
+    }
+    
+    override func viewDidLoad() {
+        
+    }
+    
+}
