b6a51b970c8c367e419a1e68cf06262fd1b887b6
crrtd <pavlov07ivan07@yandex.ru>
2024-05-23 18:18:12 +0300

    navagation controller

---
+++ ImageFeed/SingleImageViewController/SingleImageViewController.swift
@@ -0,0 +1,18 @@
+import Foundation
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    
+    var image: UIImage? {
+        didSet {
+            guard isViewLoaded else { return }
+            imageView.image = image
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imageView.image = image
+    }
+}
